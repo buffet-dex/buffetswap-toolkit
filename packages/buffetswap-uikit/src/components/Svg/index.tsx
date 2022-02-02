a96dacf47532b1d7c595bcfe09be82d5932488ad
Kepler <kepler@buffet.finance>
2022-02-02 14:33:44 -0800

    fix: UI and style updates, bump to 0.2.5

--- packages/buffetswap-uikit/src/components/Svg/index.tsx
+++ packages/buffetswap-uikit/src/components/Svg/index.tsx
@@ -12,6 +12,7 @@ export { default as ArrowLastIcon } from "./Icons/ArrowLast";
 export { default as ArrowUpIcon } from "./Icons/ArrowUp";
 export { default as ArrowUpDownIcon } from "./Icons/ArrowUpDown";
 export { default as AutoRenewIcon } from "./Icons/AutoRenew";
+export { default as AvaxIcon } from "./Icons/Avax";
 export { default as BinanceChainIcon } from "./Icons/BinanceChain";
 export { default as BinanceIcon } from "./Icons/Binance";
 export { default as BlockIcon } from "./Icons/Block";
